f0a0e09a6a3abc32f11e6807d104f6fd1cc01e83
muhuchah <m.h.chah4@gmail.com>
2024-01-03 18:25:05 +0330

    add handle_client

--- ftp_server.py
+++ ftp_server.py
@@ -3,10 +3,39 @@
 
 import socket
 import threading
+import os
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__)) + "/data"
+if not os.path.exists(BASE_DIR):
+    os.makedirs(BASE_DIR)
+    print(f"Directory '{BASE_DIR}' created successfully.")
+
+
+def handle_command(command, current_dir):
+    ...
+
+def handle_client(conn, addr):
+    current_dir = BASE_DIR
+    try:
+        while True:
+            # Receive data from the client
+            command = conn.recv(1024)
+            if not command:
+                break
+
+            response = handle_command(command, current_dir)
+            conn.sendall(response)
+
+    except Exception as e:
+        print(f"Error handling client {addr}: {e}")
+
+    finally:
+        conn.close()
+
 
 def main():
     HOST = 'localhost'
-    PORT = 21
+    PORT = 2100
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
