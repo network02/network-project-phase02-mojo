398041540776a9178e86d3bceb56370abd54634c
muhuchah <m.h.chah4@gmail.com>
2024-01-08 14:02:25 +0330

    cleaning

--- client.py
+++ client.py
@@ -6,6 +6,90 @@ PORT = 2100
 ADDR = (IP, PORT)
 FORMAT = "utf-8"
 SIZE = 1024
+SAVE_DIR = '/home/lash/Downloads/'
+
+
+def handle_stor(command, control_channel):
+    # Check if command is valid
+    if len(command.split(' ')) != 3:
+        print("STOR command is not valid.")
+        return
+
+    _, client_path, server_path = command.split(' ')
+
+    if os.path.exists(client_path):
+        file_size = os.path.getsize(client_path)
+    else:
+        print('550 File not found')
+        return
+    print(f'filesize: {file_size}')
+
+    # Send the command to the server with the size of file.
+    control_channel.sendall(f'STOR {server_path} {file_size}'.encode(FORMAT))
+
+    # Recieve data port to connect to
+    data_port = int(control_channel.recv(SIZE).decode().split(' ')[1])
+    print(f'data port: {data_port}')
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as data_channel:
+        data_channel.connect((IP, data_port))
+
+        with open(client_path, 'rb') as file:
+            while True:
+                data = file.read(SIZE)
+                if not data:
+                    break
+
+                data_channel.sendall(data)
+
+    server_response = control_channel.recv(SIZE).decode()
+    print(server_response)
+
+    # End of STOR
+
+
+def handle_retr(command, control_channel):
+    # Check if command is valid
+    if len(command.split(' ')) != 2:
+        print("RETR command is not valid.")
+        return
+
+    # Send the command to the server
+    control_channel.send(command.encode(FORMAT))
+
+    # Extract filename from the command
+    filename = SAVE_DIR + command.split(' ')[1].split('/')[-1]
+
+    # Get the port number and size of file from the server
+    # PORT {port_num} {file_size}
+    server_response = control_channel.recv(SIZE).decode()
+    if server_response.split(' ')[0] == "550":  # Check if the file exists
+        print(server_response)
+        return
+
+    data_port = int(server_response.split(' ')[2])
+    file_size = int(server_response.split(' ')[4])
+    print(f'data_port: {data_port}, file_size:{file_size}')
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as data_channel:
+        data_channel.connect((IP, data_port))
+
+        with open(filename, 'wb') as file:
+            rcv_size = 0
+
+            while True:
+                data = data_channel.recv(SIZE)
+
+                file.write(data)
+                rcv_size += len(data)
+
+                if rcv_size >= file_size:
+                    break
+    
+    server_response = control_channel.recv(SIZE).decode()
+    print(server_response)
+
+    # End of RETR
 
 
 def handle_report(control_channel):
@@ -34,6 +118,23 @@ def handle_report(control_channel):
                 break
 
     print(report)
+
+    server_response = control_channel.recv(SIZE).decode()
+    print(server_response)
+
+
+def handle_dele(command, control_channel):
+    # Check if the user is sure
+    choice = input("Do you really wish to delete y/n? ")
+    if choice == 'y' or choice == 'Y':
+        # Send the command to the server
+        control_channel.send(command.encode(FORMAT))
+
+        server_response = control_channel.recv(SIZE).decode()
+        print(server_response)
+    # Else do nothing
+
+    # End of DELE
 
 
 def main():
@@ -68,93 +169,14 @@ def main():
             command = input("Enter your command: ")
 
             if command.upper().startswith("STOR"):
-                # Check if command is valid
-                if len(command.split(' ')) != 3:
-                    print("STOR command is not valid.")
-                    continue
-
-                _, client_path, server_path = command.split(' ')
-
-                file_size = os.path.getsize(client_path)
-                print(f'filesize: {file_size}')
-
-                # Send the command to the server with the size of file.
-                client.sendall(f'STOR {server_path} {file_size}'.encode(FORMAT))
-
-                # Recieve data port to connect to
-                data_port = int(client.recv(SIZE).decode().split(' ')[1])
-                print(f'data port: {data_port}')
-
-                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as data_channel:
-                    data_channel.connect((IP, data_port))
-
-                    with open(client_path, 'rb') as file:
-                        while True:
-                            data = file.read(SIZE)
-                            if not data:
-                                break
-
-                            data_channel.sendall(data)
-                
-                server_response = client.recv(SIZE).decode()
-                print(server_response)
-
-                # End of STOR
+                handle_stor(command=command, control_channel=client)
             elif command.upper().startswith("RETR"):
-                # Check if command is valid
-                if len(command.split(' ')) != 2:
-                    print("RETR command is not valid.")
-                    continue
+                handle_retr(command=command, control_channel=client)
 
-                # Send the command to the server
-                client.send(command.encode(FORMAT))
-
-                # Extract filename from the command
-                filename = '/home/lash/Downloads/' + command.split(' ')[1].split('/')[-1]
-
-                # Get the port number and size of file from the server
-                # PORT {port_num} {file_size}
-                server_response = client.recv(SIZE).decode()
-                data_port = int(server_response.split(' ')[1])
-                file_size = int(server_response.split(' ')[2])
-                print(f'data_port: {data_port}, file_size:{file_size}')
-
-                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as data_channel:
-                    data_channel.connect((IP, data_port))
-
-                    with open(filename, 'wb') as file:
-                        rcv_size = 0
-
-                        while True:
-                            data = data_channel.recv(SIZE)
-
-                            file.write(data)
-                            rcv_size += len(data)
-
-                            if rcv_size >= file_size:
-                                break
-                
-                server_response = client.recv(SIZE).decode()
-                print(server_response)
-
-                # End of RETR
             elif command.upper().startswith("DELE"):
-                # Check if the user is sure
-                choice = input("Do you really wish to delete y/n? ")
-                if choice == 'y' or choice == 'Y':
-                    # Send the command to the server
-                    client.send(command.encode(FORMAT))
-
-                    server_response = client.recv(SIZE).decode()
-                    print(server_response)
-                # Else do nothing
-    
-                # End of DELE
+                handle_dele(command=command, control_channel=client)
             elif command.upper().startswith("REPORT"):
                 handle_report(control_channel=client)
-
-                server_response = client.recv(SIZE).decode()
-                print(server_response)
             elif command.upper().startswith("QUIT"):
                 # Send the command to the server
                 client.send(command.encode(FORMAT))
